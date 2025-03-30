178a48efe6e379dd2271d783f7b1c85ba3987106
Weegoos <batyr.ashim05@mail.ru>
2025-03-30 09:21:32 +0500

    CSIA: The ViewDetailedInformationAboutPlant.vue has been created and integrated to the project

---
+++ src/components/myPlant.vue/ViewDetailedInformationAboutPlant.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <q-dialog class="text-black" v-model="confirm" persistent>
+      <q-card>
+        <q-card-section class="row items-center">
+          <q-avatar icon="signal_wifi_off" color="primary" text-color="white" />
+          <span class="q-ml-sm">{{ props.allInfoAboutPlant }}</span>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" color="primary" v-close-popup />
+          <q-btn flat label="Turn on Wifi" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script setup>
+import { useQuasar } from "quasar";
+import { getCurrentInstance, ref, watch } from "vue";
+
+// global variables
+const { proxy } = getCurrentInstance();
+const serverURL = proxy.$serverURL;
+const $q = useQuasar();
+const props = defineProps({
+  openWindow: {
+    type: Boolean,
+    default: false,
+  },
+  allInfoAboutPlant: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+
+const confirm = ref(props.openWindow);
+
+watch(
+  () => props.openWindow,
+  (newValue) => {
+    confirm.value = newValue;
+  }
+);
+</script>
+
+<style></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,11 +2,17 @@
   <div>
     <section align="right">
       <q-btn
-        class="bg-grey"
+        class="bg-grey q-mr-sm"
         round
         color="black"
         icon="mdi-bell-outline"
         @click="pushToNofications"
+      />
+      <q-btn
+        style="background-color: #30322e"
+        round
+        icon="mdi-book-open-outline"
+        @click="pushToGuide"
       />
     </section>
     <p class="text-h5 text-bold">Good day</p>
@@ -14,11 +20,44 @@
     <section
       class="q-pa-sm q-my-md"
       style="background-color: #30322e; border-radius: 8px"
+      v-if="allMyPlants == []"
     >
       <p class="text-body2 text-bold">Add your first plant</p>
       <p>
         Planta will then create a care schedule for it to make sure it thrives
       </p>
+    </section>
+    <section v-else>
+      <div>
+        <q-list bordered>
+          <q-item
+            clickable
+            v-ripple
+            v-for="(plant, index) in allMyPlants.content"
+            :key="index"
+            @click="openDetailedMyPlant(plant)"
+          >
+            <q-item-section avatar class="text-white">
+              <q-icon color="green-4" name="mdi-leaf" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label class="text-body1 text-bold">{{
+                plant.custom_name
+              }}</q-item-label>
+              <q-item-label class="text-white" caption lines="2">{{
+                plant.substrate
+              }}</q-item-label>
+              <q-item-label class="text-white" caption lines="2">
+                Collect time:
+                {{ plant.collectTime }}
+              </q-item-label>
+            </q-item-section>
+            <q-item-section side center>
+              <q-icon name="mdi-arrow-right" color="white" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
     </section>
     <q-btn
       color="green-4 q-mt-md"
@@ -28,24 +67,26 @@
       no-caps
       @click="addPlant"
     />
-    <div class="q-ma-md absolute-bottom-right" style="z-index: 10">
-      <q-btn
-        style="background-color: #30322e"
-        round
-        icon="mdi-book-open-outline"
-        @click="pushToGuide"
-      />
-    </div>
+    <ViewDetailedInformationAboutPlant
+      :openWindow="openWindow"
+      :allInfoAboutPlant="allInfoAboutPlant"
+    />
   </div>
 </template>
 
 <script setup>
+import { useQuasar } from "quasar";
+import ViewDetailedInformationAboutPlant from "src/components/myPlant.vue/ViewDetailedInformationAboutPlant.vue";
+import { getMethod } from "src/composables/apiMethod/get";
 import { checkAccessToken } from "src/composables/javascript-function/token";
-import { onMounted } from "vue";
+import { getCurrentInstance, onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 
 // global variables
 const router = useRouter();
+const { proxy } = getCurrentInstance();
+const serverURL = proxy.$serverURL;
+const $q = useQuasar();
 
 const pushToNofications = () => {
   router.push("/notifications");
@@ -55,13 +96,34 @@ const addPlant = () => {
   router.push("/create-plant");
 };
 
-onMounted(() => {
-  checkAccessToken();
-});
-
 const pushToGuide = () => {
   router.push("/guide");
 };
+
+const allInfoAboutPlant = ref([]);
+const openWindow = ref(false);
+const openDetailedMyPlant = (info) => {
+  allInfoAboutPlant.value = info;
+  openWindow.value = true;
+};
+
+const allMyPlants = ref([]);
+const getAllMyPlants = async () => {
+  await getMethod(
+    serverURL,
+    `user/myPlants?page=0&size=10`,
+    allMyPlants,
+    $q,
+    "Error: "
+  );
+
+  console.log(allMyPlants.value);
+};
+
+onMounted(() => {
+  getAllMyPlants();
+  checkAccessToken();
+});
 </script>
 
 <style></style>
