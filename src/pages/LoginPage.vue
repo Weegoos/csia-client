35d0af63d2a8eb4780b9f0e2799329c0a5c1aead
Weegoos <batyr.ashim05@mail.ru>
2025-04-06 14:10:51 +0500

    CSIA: The LoginPage.test.js has been updated

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -4,13 +4,16 @@
     :class="$q.screen.width > mobileWidth ? 'fixed-center' : ''"
   >
     <section class="text-white q-mb-md">
-      <p class="text-h5 text-bold">Log in to the system</p>
-      <p class="text-body2">
+      <p class="text-h5 text-bold" data-testid="loginMainText">
+        Log in to the system
+      </p>
+      <p class="text-body2" data-testid="loginSubText">
         Please enter your credentials to access your account
       </p>
     </section>
     <section>
       <q-input
+        data-testid="emailInput"
         v-model="email"
         placeholder="Enter your email"
         stack-label
@@ -23,6 +26,7 @@
         outlined
       />
       <q-input
+        data-testid="passwordInput"
         v-model="password"
         :type="isPwd ? 'password' : 'text'"
         placeholder="Enter your password"
@@ -64,52 +68,6 @@
     </section>
   </div>
 </template>
-
-<!-- <script setup>
-import axios from "axios";
-import { Cookies, useQuasar } from "quasar";
-import { useNotifyStore } from "src/stores/notify-store";
-import { getCurrentInstance, ref } from "vue";
-import { useRouter } from "vue-router";
-
-// global variables
-const router = useRouter();
-const { proxy } = getCurrentInstance();
-const serverURL = proxy.$serverURL;
-const mobileWidth = proxy.$mobileWidth;
-const notifyStore = useNotifyStore();
-const $q = useQuasar();
-
-const isPwd = ref(true);
-const email = ref("");
-const password = ref("");
-
-const login = async () => {
-  const payload = {
-    email: email.value,
-    password: password.value,
-  };
-  try {
-    const response = await axios.post(`${serverURL}auth/login`, payload, {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    });
-    console.log(response.data);
-    Cookies.set("accessToken", response.data.accessToken);
-    Cookies.set("refreshToken", response.data.refreshToken);
-    notifyStore.nofifySuccess($q, "The user has successfully logged in");
-    router.push("/");
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const pushToRegistration = () => {
-  router.push("/registration");
-};
-</script> -->
 
 <script>
 import axios from "axios";
--- test/vitest/__tests__/LoginPage.test.js
+++ test/vitest/__tests__/LoginPage.test.js
@@ -3,7 +3,7 @@ import { mount } from "@vue/test-utils";
 import { createPinia } from "pinia";
 import { Quasar } from "quasar";
 import LoginPage from "src/pages/LoginPage.vue";
-import { describe, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 
 installQuasarPlugin();
 
@@ -15,7 +15,33 @@ describe("tests for LoginPage", () => {
       plugins: [pinia, Quasar],
     },
   });
-  it("", () => {
-    // Test logic for LoginPage
+  it("should find loginMainText data-testid", () => {
+    const loginMainText = wrapper.find('[data-testid="loginMainText"]');
+    expect(loginMainText.exists()).toBe(true);
+    expect(loginMainText.text()).toContain("Log in");
+    expect(loginMainText).toMatchSnapshot();
+  });
+
+  it("should find loginSubText data-testid", () => {
+    const loginSubText = wrapper.find('[data-testid="loginSubText"]');
+    expect(loginSubText.exists()).toBe(true);
+    expect(loginSubText.text()).toContain("Please enter your credentials");
+    expect(loginSubText).toMatchSnapshot();
+  });
+
+  it("should find emailInput data-testid", () => {
+    const emailInput = wrapper.find('[data-testid="emailInput"]');
+    expect(emailInput.exists()).toBe(true);
+
+    emailInput.setValue("example@mail.ru");
+    expect(emailInput.element.value).toBe("example@mail.ru");
+  });
+
+  it("should find passwordInput data-testid", () => {
+    const passwordInput = wrapper.find('[data-testid="passwordInput"]');
+    expect(passwordInput.exists()).toBe(true);
+
+    passwordInput.setValue("examplePassword");
+    expect(passwordInput.element.value).toBe("examplePassword");
   });
 });
