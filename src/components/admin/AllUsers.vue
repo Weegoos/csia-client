a5ec2fc1bc39739d839cbd471c9b924accdf5702
Weegoos <batyr.ashim05@mail.ru>
2025-04-15 16:04:56 +0500

    Tennis: The AllUSers.vue has been updated

--- src/components/admin/AllUsers.vue
+++ src/components/admin/AllUsers.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="q-pa-sm">
     <q-table
+      @row-click="openDetailedInfo"
       flat
       dark
       bordered
@@ -10,10 +11,13 @@
       row-key="name"
       hide-bottom
     >
-      <template v-slot:body-cell-actions>
+      <template v-slot:body-cell-avatar="props">
         <q-td align="center">
-          <q-avatar size="60px" font-size="52px">
-            <img src="https://picsum.photos/500/300" alt="" />
+          <q-avatar size="60px" v-if="props.row.photoUrl">
+            <img :src="props.row.photoUrl" alt="User Image" />
+          </q-avatar>
+          <q-avatar size="60px" color="grey-4" text-color="black" v-else>
+            ?
           </q-avatar>
         </q-td>
       </template>
@@ -23,18 +27,25 @@
             flat
             color="red-4"
             icon="mdi-delete"
-            @click="deleteUser(props)"
+            @click="deleteUser(props.row)"
           />
         </q-td>
       </template>
     </q-table>
+    <DetailedInformationAboutUsers
+      :isOpenedDetailedInfoAboutUser="Boolean(isOpenedDetailedInfoAboutUser)"
+      @closeDetailedInformationAboutUser="closeDetailedInformationAboutUser"
+      :allInfoAboutUser="Object(allInfoAboutUser)"
+    />
   </div>
 </template>
 
 <script setup>
-import { useQuasar } from "quasar";
+import { Cookies, useQuasar } from "quasar";
+import { deleteMethod } from "src/composables/apiMethod/delete";
 import { getMethod } from "src/composables/apiMethod/get";
 import { getCurrentInstance, onMounted, ref } from "vue";
+import DetailedInformationAboutUsers from "./DetailedInformationAboutUsers.vue";
 
 // global variables
 const { proxy } = getCurrentInstance();
@@ -43,10 +54,10 @@ const $q = useQuasar();
 
 const columns = [
   {
-    name: "actions",
-    label: "Image",
-    align: "left",
-    field: "id",
+    name: "avatar",
+    label: "Фото",
+    field: "photoUrl",
+    align: "center",
   },
   {
     name: "id",
@@ -82,6 +93,7 @@ const columns = [
 
 const allUsers = ref([]);
 const rows = ref([]);
+
 const getAllUsers = async () => {
   try {
     await getMethod(
@@ -91,12 +103,62 @@ const getAllUsers = async () => {
       $q,
       "Ошибка при получении всех пользователей: "
     );
-    // console.log(allUsers.value);
-    rows.value = allUsers.value.content;
-    console.log(rows.value);
+
+    const users = allUsers.value.content ?? [];
+
+    const usersWithPhotos = await Promise.all(
+      users.map(async (user) => {
+        if (user.profileImage) {
+          try {
+            const response = await fetch(
+              `${serverURL}user/image/${user.profileImage}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${Cookies.get("accessToken")}`, // или cookies
+                },
+              }
+            );
+            if (!response.ok)
+              throw new Error("Не удалось получить изображение");
+
+            const blob = await response.blob();
+            const imageUrl = URL.createObjectURL(blob);
+            return { ...user, photoUrl: imageUrl };
+          } catch (err) {
+            console.warn(`Ошибка загрузки фото для user ${user.id}`, err);
+            return { ...user, photoUrl: null };
+          }
+        } else {
+          return { ...user, photoUrl: null };
+        }
+      })
+    );
+
+    rows.value = usersWithPhotos;
   } catch (error) {
     console.error(error);
   }
+};
+
+const deleteUser = async (row) => {
+  try {
+    deleteMethod(serverURL, "user", `delete?id=${row.id}`);
+    console.log(row);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const isOpenedDetailedInfoAboutUser = ref(false);
+const allInfoAboutUser = ref([]);
+const openDetailedInfo = (err, row, instance) => {
+  console.log(row);
+  isOpenedDetailedInfoAboutUser.value = true;
+  allInfoAboutUser.value = row;
+};
+
+const closeDetailedInformationAboutUser = () => {
+  isOpenedDetailedInfoAboutUser.value = false;
 };
 
 onMounted(() => {
---
+++ src/components/admin/DetailedInformationAboutUsers.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <q-dialog v-model="confirm" persistent>
+      <q-card dark>
+        <q-card-section class="row items-center">
+          {{ props.allInfoAboutUser }}
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn
+            flat
+            label="Cancel"
+            color="primary"
+            @click="closeDetailedInformationAboutUser"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+
+// global variables
+const props = defineProps({
+  isOpenedDetailedInfoAboutUser: {
+    default: false,
+    type: Boolean,
+    required: true,
+  },
+  allInfoAboutUser: {
+    type: Object,
+    required: true,
+  },
+});
+const confirm = ref(props.isOpenedDetailedInfoAboutUser);
+
+watch(
+  () => props.isOpenedDetailedInfoAboutUser,
+  (newVal) => {
+    confirm.value = newVal;
+  }
+);
+
+const emit = defineEmits(["closeDetailedInformationAboutUser"]);
+const closeDetailedInformationAboutUser = () => {
+  emit("closeDetailedInformationAboutUser");
+};
+</script>
+
+<style></style>
