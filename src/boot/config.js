02835e1580eae4a9dd759a1778f5dd350011f7c3
Weegoos <batyr.ashim05@mail.ru>
2025-04-18 11:34:03 +0500

    Tennis: The deploy of the project

--- src/boot/config.js
+++ src/boot/config.js
@@ -1,7 +1,7 @@
 // src/boot/config.js
 export default ({ app }) => {
   const serverURL = "https://csia-back.onrender.com/";
-  const clientURL = "http://localhost:9000/#/";
+  const clientURL = "https://kazplant.netlify.app/#/";
   const maxItemsPerPage = 10;
   const mobileWidth = 800;
 
